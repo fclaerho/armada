38b1407787e36ca48d9ff5ab3106871db7c5d45f
florent claerhout <florent.claerhout@gmail.com>
2012-12-05 20:13:30 +0100

    Replace assert with test, fix wait result

--- armada.c
+++ armada.c
@@ -1,36 +1,38 @@
 /* Copyright (c) 2012 f.claerhout, licensed under the GPL */
 
 #include <unistd.h>
-#include <assert.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-#ifdef NDEBUG
-	#error Assertions cannot be disabled. Undefine NDEBUG and try again.
-#endif
+void test(_Bool b) {
+	if(b) return;
+	perror("fatal");
+	abort();
+}
 
 int main(int argc, char **argv) {
 	char name[L_tmpnam + 2];
-	assert(tmpnam(name));
-	strcat(name, ".c");
+	test(tmpnam(name));
+	(void)strcat(name, ".c");
 	FILE *file = fopen(name, "a+");
-	assert(file);
-	assert(fputs("#include <stdlib.h>\n#include <stdio.h>\nint main(int argc, char **argv) { ", file) != EOF);
+	test(file);
+	test(fputs("#include <stdlib.h>\n#include <stdio.h>\nint main(int argc, char **argv) { ", file) != EOF);
 	char text[1024];
-	while(fgets(text, sizeof(text), stdin)) fputs(text, file);
-	fputs("}\n", file);
+	while(fgets(text, sizeof(text), stdin)) test(fputs(text, file) != EOF);
+	test(fputs("}\n", file) != EOF);
 	rewind(file);
 	pid_t pid = fork();
-	assert(pid != -1);
-	if(!pid) assert(execlp("c99", "c99", "-O3", name, 0) != -1);
-	int code;
-	assert(wait(&code) == pid && !code);
-	assert(remove(name) != -1);
+	test(pid != -1);
+	if(!pid) test(execlp("c99", "c99", "-O3", name, 0) != -1);
+	int stat;
+	test(wait(&stat) != -1);
+	test(WIFEXITED(stat) && !WEXITSTATUS(stat));
+	test(remove(name) != -1);
 	pid = fork();
-	assert(pid != -1);
-	if(!pid) assert(execvp("./a.out", argv) != -1);
-	assert(wait(&code) == pid);
-	assert(remove("./a.out") != -1);
-	return code;
+	test(pid != -1);
+	if(!pid) test(execvp("./a.out", argv) != -1);
+	test(wait(&stat) != -1);
+	test(remove("./a.out") != -1);
+	return WIFEXITED(stat)? WEXITSTATUS(stat): EXIT_FAILURE;
 }
